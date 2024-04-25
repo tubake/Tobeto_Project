6430e6ae6267c799c0758d929be849dff65fa0c5
Tuba Bezek <tuba.bezek@bil.omu.edu.tr>
2024-04-25 23:03:24 +0300

    files removed

--- userInfo.py
+++
@@ -1,9 +0,0 @@
-firstName= "Pair"
-lastName= "Beş"
-email="tobetopair5@gmail.com"
-password="123456789"
-passwordAgain="123456789"
-telephone="05365685626"
-
-email_tuba= "mihraa777@gmail.com"
-password_tuba="123456"
